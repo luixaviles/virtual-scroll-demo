453fedb71e62b62188f87fa281f6b25246ad6c92
luixaviles <luis.avilesz@gmail.com>
2018-12-01 02:47:39 -0400

    Minor fix

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,12 +2,7 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { TempComponent } from './temp/temp.component';
 
-const routes: Routes = [
-  {
-    path: '',
-    component: TempComponent
-  }
-];
+const routes: Routes = [];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
