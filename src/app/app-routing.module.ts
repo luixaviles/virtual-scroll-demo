4a9a188b3e3162560b8315b7127e66a03cb7c5b6
luixaviles <luis.avilesz@gmail.com>
2018-12-01 09:55:52 -0400

    Removing unused import

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { TempComponent } from './temp/temp.component';
 
 const routes: Routes = [];
 
